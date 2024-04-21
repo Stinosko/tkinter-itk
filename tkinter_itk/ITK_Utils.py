5a498a45971be71e42da5718f6c0ceff1ff707ce
Stijn Schatteman <stijn.schatteman@gmail.com>
2024-04-21 21:28:10 +0200

    Improve ITK_utils

--- tkinter_itk/ITK_Utils.py
+++ tkinter_itk/ITK_Utils.py
@@ -5,6 +5,10 @@ import pandas as pd
 import cv2
 import logging 
 from PIL import Image
+import nibabel as nib
+import pydicom
+from pydicom.uid import generate_uid
+import time
 
 def read_image(image_path):
     """
@@ -12,7 +16,7 @@ def read_image(image_path):
     :param image_path: path to the image
     :return: the image
     """
-    image = sitk.ReadImage(image)
+    image = sitk.ReadImage(image_path)
     return image
 
 
@@ -38,7 +42,7 @@ def read_dicom_series(dicom_series_path):
     return image
 
 
-def overlay_segmentation(image, segmentation, alpha=0.5):
+def overlay_segmentation(image: sitk.Image, segmentation, alpha=0.5):
     """
     Overlay a segmentation on an image
     :param image: the image
@@ -46,7 +50,10 @@ def overlay_segmentation(image, segmentation, alpha=0.5):
     :param alpha: the transparency of the segmentation
     :return: the overlaid image
     """
-    return sitk.LabelOverlay(image, segmentation, alpha)
+    if image.GetPixelID() != sitk.sitkUInt8:
+        logging.warning("Casting image to UInt8")
+        image = sitk.Cast(image, sitk.sitkUInt8)
+    return sitk.Cast( sitk.LabelOverlay(image, segmentation, alpha), sitk.sitkVectorUInt8)
 
 
 def get_PIL_image_with_resolution(image, resolution):
@@ -90,7 +97,7 @@ def get_stats_of_segmentation(segmentation_image: sitk.Image, input_image: sitk.
     # Get highest number of segmentation label
     # Because some segmentation does not have all the labels
     # So shape_stats.GetNumberOfLabels() is not reliable
-    amount = sitk.GetArrayFromImage(segmentation_image).max()
+    amount = int(np.max(sitk.GetArrayFromImage(segmentation_image))) + 1
 
     stats_list = []
 
@@ -105,7 +112,6 @@ def get_stats_of_segmentation(segmentation_image: sitk.Image, input_image: sitk.
                                 intensity_stats.GetStandardDeviation(i),
                                 intensity_stats.GetSkewness(i), 
                                 intensity_stats.GetMinimum(i),
-
                                 intensity_stats.GetMaximum(i),))
         else:
                 stats_list.append((0,0,0,0,0,0,0,0)) 
@@ -238,12 +244,15 @@ def text_to_image(text, RGB = False, size_x = 512, slice_number = 0):
     return seg_info_image
 
 
-def place_text_above_image(input_image, text, RGB = False):
+def place_text_above_image(input_image: sitk.Image, text, RGB = False):
+    if input_image.GetPixelID() == sitk.sitkVectorUInt8 and not RGB:
+        logging.warning("input_image is Vector 8 image, but RGB is set to False, setting RGB to True")
+        RGB = True
+    
     seg_info_image = text_to_image(text, RGB)
 
     # combine seg_info_image and input_image
     size = list(input_image.GetSize())
-    
     
     if size[0] < seg_info_image.GetSize()[0]:
         size[0] = seg_info_image.GetSize()[0]
@@ -292,28 +301,34 @@ def isolate_region_based_on_segmentation(image: sitk.Image, segmentation_image, 
         raise ValueError("Image dimension is not 2 or 3")
     return isolated_image
 
+def GetGDCMSeriesIDs_recursive(DICOM_DIR, reader):
+    """Get the series IDs of a DICOM directory"""
+    result = tuple()
+    result = reader.GetGDCMSeriesIDs(DICOM_DIR)
+    for directory in [ f.path for f in os.scandir(DICOM_DIR) if f.is_dir() ]:
+        temp = GetGDCMSeriesIDs_recursive(directory, reader)
+        if len(temp) > 0:
+            result = result + temp
+    return result
 
 
-def pydicom_2_sitk(pd_image):
+# credit to https://niftynet.readthedocs.io/en/v0.6.0/_modules/niftynet/io/image_loader.html
+def pydicom_2_sitk(pd_image: pydicom.dataset.FileDataset):
     """Convert a pydicom image to a SimpleITK image"""
-    # combine the pixel arrays
     array = np.empty((len(pd_image), pd_image[0].pixel_array.shape[0], pd_image[0].pixel_array.shape[1]))
     for i, item in enumerate( pd_image):
         array[i, :, :] = item.pixel_array
-    # print(array.shape)
     array = array + pd_image[0].RescaleIntercept
     array[array < pd_image[0].RescaleIntercept] = pd_image[0].RescaleIntercept
     # convert to SimpleITK image
     image = sitk.GetImageFromArray(array)
-    # set the origin, spacing and direction
-    # print("origin: ", pd_image[0].get((0x0020, 0x0032)).value)
     image.SetOrigin(pd_image[0].get((0x0020, 0x0032)).value)
+
     spacing = pd_image[0].PixelSpacing
-    slice_thickness = pd_image[0].get((0x0018, 0x0050)).value
-    # print("spacing: ", spacing)
-    # print("slice_thickness: ", slice_thickness)
-    image.SetSpacing([spacing[0], spacing[1], slice_thickness])
-    # print(pd_image[0].get((0x0020, 0x0037)).value)
+
+    slice_spacing = pd_image[0].get((0x0018, 0x0050)).value
+
+    image.SetSpacing([spacing[0], spacing[1], slice_spacing])
     x, y = pd_image[0].get((0x0020, 0x0037)).value[0:3], pd_image[0].get((0x0020, 0x0037)).value[3:6]
     origin_start = pd_image[0].get((0x0020, 0x0032)).value
     origin_end = pd_image[-1].get((0x0020, 0x0032)).value
@@ -321,3 +336,196 @@ def pydicom_2_sitk(pd_image):
     z = z / np.linalg.norm(z)
     image.SetDirection([x[0], y[0], z[0], x[1], y[1], z[1], x[2], y[2], z[2]])
     return image
+
+def sitk_2_pydicom(sitk_image: sitk.Image):
+    """Convert a SimpleITK image to a pydicom image"""
+    # convert to numpy array
+    image = sitk.GetArrayFromImage(sitk_image)
+    # convert to pydicom image
+    image = pydicom.dataset.FileDataset()
+    image.PixelData = image.tobytes()
+    # set the origin, spacing and direction
+    image.Origin = sitk_image.GetOrigin()
+    image.Spacing = sitk_image.GetSpacing()
+    image.Direction = sitk_image.GetDirection()
+    return image
+
+# https://niftynet.readthedocs.io/en/v0.6.0/_modules/niftynet/io/image_loader.html#imread_sitk
+def make_affine_from_sitk(sitk_img):
+    """Get affine transform in LPS"""
+    if sitk_img.GetDepth() <= 0:
+        return np.eye(4)
+
+    rot = [sitk_img.TransformContinuousIndexToPhysicalPoint(p)
+           for p in ((1, 0, 0),
+                     (0, 1, 0),
+                     (0, 0, 1),
+                     (0, 0, 0))]
+    rot = np.array(rot)
+    affine = np.concatenate([
+        np.concatenate([rot[0:3] - rot[3:], rot[3:]], axis=0),
+        [[0.], [0.], [0.], [1.]]
+    ], axis=1)
+    affine = np.transpose(affine)
+    # convert to RAS to match nibabel
+    affine = np.matmul(np.diag([-1., -1., 1., 1.]), affine)
+    return affine
+
+def sitk_2_nib(sitk_image: sitk.Image):
+    """Convert a SimpleITK image to a nibabel image"""
+    # convert to numpy array
+    image = sitk.GetArrayFromImage(sitk_image)
+    if sitk_image.GetDimension() > 2:
+        image = image.transpose()
+    # convert to nibabel image
+    nib_image = nib.Nifti1Image(image, make_affine_from_sitk(sitk_image))
+    # nib_image.header.set_zooms(sitk_image.GetSpacing())
+    return nib_image
+
+def nib_2_sitk(nib_image: nib.Nifti1Image):
+    """Convert a nibabel image to a SimpleITK image"""
+    # convert to numpy array
+    image = nib_image.get_fdata()
+    if len(image.shape) >= 3:
+        image = image.transpose()
+    # convert to SimpleITK image
+    image = sitk.GetImageFromArray(image)
+    # set the origin, spacing and direction
+    image.SetOrigin(nib_image.affine[0:3, 3])
+    image.SetSpacing([float(x) for x in nib_image.header.get_zooms()])
+    direction = nib_image.affine[0:3, 0:3]
+    direction = np.matmul(np.diag([-1., -1., 1.]), direction)
+    # normalise each row
+    direction = direction / np.linalg.norm(direction, axis=1)[:, None]
+    image.SetDirection(direction.flatten())
+    return image
+
+
+def write_2d_image(series_tag_values, image, out_dir, modality, writer, slice_number = 0, custom_tags = None):
+    if modality is None:
+        modality = "CT"
+    
+    if custom_tags is not None:
+        for tag, value in custom_tags.items():
+            # Check if the tag is already present
+            for tag_value in series_tag_values:
+                if tag_value[0] == tag:
+                    series_tag_values.remove(tag_value)
+                    break
+            series_tag_values.append((tag, value))
+    
+    # Tags shared by the series.
+    for tag, value in series_tag_values:
+        # logging.info(f"Setting tag {tag} to {value}")
+        try:
+            image.SetMetaData(tag, value)
+        except TypeError:
+            logging.error(f"Type Error setting tag {tag} to {value}, type {type(value)}")
+    
+    # Slice specific tags.
+    #   Instance Creation Date
+    image.SetMetaData("0008|0012", time.strftime("%Y%m%d"))
+    #   Instance Creation Time
+    image.SetMetaData("0008|0013", time.strftime("%H%M%S"))
+
+    # Setting the type to CT so that the slice location is preserved and
+    # the thickness is carried over.
+    image.SetMetaData("0008|0060", modality)
+
+    # (0020, 0032) image position patient determines the 3D spacing between
+    # slices.
+    #   Image Position (Patient)
+    image.SetMetaData(
+        "0020|0032",
+        "\\".join(map(str, image.TransformIndexToPhysicalPoint((0, 0)))),
+    )
+    #   Instance Number
+    image.SetMetaData("0020|0013", str(slice_number))
+
+    # Write to the output directory and add the extension dcm, to force
+    # writing in DICOM format.
+    writer.SetFileName(os.path.join(out_dir, str(slice_number) + ".dcm"))
+    writer.Execute(image)
+
+def writeSlice(series_tag_values, new_img, out_dir, i, modality, writer):
+    img = new_img[:,:,i]
+    img.SetOrigin(new_img.TransformIndexToPhysicalPoint((0, 0, i)))
+    
+    write_2d_image(series_tag_values, img, out_dir, modality, writer, slice_number=i)
+
+# save as DICOM
+def write_3d_image(image, folder_name, name, modality, level = None, window = None, custom_tags = None):
+    writer = sitk.ImageFileWriter()
+    # Use the study/series/frame of reference information given in the meta-data
+    # dictionary and not the automatically generated information from the file IO
+    writer.KeepOriginalImageUIDOn()
+
+    modification_time = time.strftime("%H%M%S")
+    modification_date = time.strftime("%Y%m%d")
+
+    # Copy some of the tags and add the relevant tags indicating the change.
+    # For the series instance UID (0020|000e), each of the components is a number,
+    # cannot start with zero, and separated by a '.' We create a unique series ID
+    # using the date and time. Tags of interest:
+    direction = image.GetDirection()
+    
+    series_tag_values = [
+        ("0008|0031", modification_time),  # Series Time
+        ("0008|0021", modification_date),  # Series Date
+        ("0008|0008", "DERIVED\\SECONDARY"),  # Image Type
+        ("0020|000e", generate_uid()),  # Series Instance UID
+        (
+            "0020|0037",
+            "\\".join(
+                map(
+                    str,
+                    (
+                        direction[0],
+                        direction[3],
+                        direction[6],
+                        direction[1],
+                        direction[4],
+                        direction[7],
+                    ),
+                )
+            ),
+        ),  # Image Orientation
+        # (Patient)
+        ("0008|103e", name),  # Series Description
+    ]
+    # logging.info(series_tag_values)
+    # Add custom tags
+    if custom_tags is not None:
+        for tag, value in custom_tags.items():
+            # Check if the tag is already present
+            for tag_value in series_tag_values:
+                if tag_value[0] == tag:
+                    series_tag_values.remove(tag_value)
+                    break
+            series_tag_values.append((tag, value))
+    # logging.info(series_tag_values)
+
+    if level is not None:
+        series_tag_values.append(("0028|1050", str(level)))
+    if window is not None:
+        series_tag_values.append(("0028|1051", str(window)))
+
+    if not os.path.exists(folder_name):
+        # If it doesn't exist, create it
+        os.makedirs(folder_name)
+    # Write slices to output directory
+    list(
+        map(
+            lambda i: writeSlice(series_tag_values, image, folder_name, i, modality, writer= writer),
+            range(image.GetDepth()),
+        )
+    )
+
+
+def save_itk_as_dicom(image, folder_name, modality, level = None, window = None, custom_tags = None):
+    """Save an ITK image as a DICOM series"""
+    # Write the image
+    if image.GetDimension() == 2:
+        write_2d_image(image, folder_name, modality, level, window, custom_tags)
+    elif image.GetDimension() == 3:
+        write_3d_image(image, folder_name, "image", modality, level, window, custom_tags)
