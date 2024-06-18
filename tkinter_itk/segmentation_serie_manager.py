af678cb36b3eba3891969cb084f7bdf917440dc2
Stijn Schatteman <stijn.schatteman@gmail.com>
2024-06-18 22:05:36 +0200

    support multiple seg_images for one serie

--- tkinter_itk/segmentation_serie_manager.py
+++ tkinter_itk/segmentation_serie_manager.py
@@ -10,80 +10,156 @@ class Segmentation_serie_manager:
         self.segmentation_images = {}
         self.segmentation_stats = {}
         self.segmentation_preview_images = {}
+        self.seg_label_dict : dict = {} # dictionary to store the labels of the segmentation
 
-    def add_segmentation_serie(self, serie_ID):
-        if serie_ID in self.segmentation_images:
-            logging.warning('Serie ID already exists')
+    def add_segmentation_serie(self, serie_ID, name = "default", label_dict = None):
+        if serie_ID not in self.DICOM_manager.get_serie_IDs():
+            logging.error('Serie ID not found')
             return
-        self.segmentation_images[serie_ID] = sitk.Image(self.DICOM_manager.get_serie_size(serie_ID), sitk.sitkUInt8)
-        self.segmentation_images[serie_ID].CopyInformation(self.DICOM_manager.get_serie_image(serie_ID))
     
-    def get_segmentation_IDs(self, add_if_not_exist=False):
-        return self.segmentation_images.keys()
+        if serie_ID not in self.segmentation_images:
+            self.segmentation_images[serie_ID] = {}
+            self.segmentation_stats[serie_ID] = {}
+        
+        if name in self.segmentation_images[serie_ID]:
+            logging.warning('Segmentation name already exists, overwriting')
+            
+        self.segmentation_images[serie_ID] = {}
+        self.segmentation_images[serie_ID][name] = sitk.Image(self.DICOM_manager.get_serie_size(serie_ID), sitk.sitkUInt8)
+        self.segmentation_images[serie_ID][name].CopyInformation(self.DICOM_manager.get_serie_image(serie_ID))
 
-    def get_serie_length(self, serie_ID):
-        return self.segmentation_images[serie_ID].GetSize()[2]
+        self.set_label_dict(serie_ID, label_dict)
+
+
+    @staticmethod
+    def label_dict_is_valid(label_dict):
+        if label_dict is None:
+            return False
+        if not isinstance(label_dict, dict):
+            return False
+        for key in label_dict:
+            if not isinstance(key, int):
+                return False
+            if not isinstance(label_dict[key], str):
+                return False
+        return True
+
+
+    def get_segmentation_IDs(self, serie_ID):
+        return self.segmentation_images[serie_ID].keys()
+
+    def get_serie_length(self, serie_ID, name = "default"):
+        return self.segmentation_images[serie_ID][name].GetSize()[2]
     
-    def get_image(self, serie_ID, add_if_not_exist=False):
-        if add_if_not_exist and serie_ID not in self.segmentation_images:
-            self.add_segmentation_serie(serie_ID)
-        return self.segmentation_images[serie_ID]
+    def get_image(self, serie_ID, name = "default", add_if_not_exist=False):
+        if add_if_not_exist and serie_ID not in self.segmentation_images or name not in self.segmentation_images[serie_ID]:
+            self.add_segmentation_serie(serie_ID, name)
+        return self.segmentation_images[serie_ID][name]
     
-    def get_segmentation(self, serie_ID, add_if_not_exist=False):
-        return self.get_image(serie_ID, add_if_not_exist)
+    def get_segmentation(self, serie_ID, name = "default", add_if_not_exist=False):
+        return self.get_image(serie_ID, name, add_if_not_exist)
 
     def save_segmentations(self, location):
         for serie_ID in self.segmentation_images:
-            sitk.WriteImage(self.segmentation_images[serie_ID], os.path.join(location, serie_ID + '.nii.gz'))
+            for segmentation_name in self.segmentation_images[serie_ID]:
+                logging.info('Saving segmentation: ' + serie_ID)
+                sitk.WriteImage(self.segmentation_images[serie_ID][segmentation_name], 
+                                os.path.join(location, serie_ID , segmentation_name + '.nii.gz'))
 
     def load_segmentations(self, location):
         for file in os.listdir(location):
             if file.endswith('.nii.gz') and file[:-7] in self.DICOM_manager.get_serie_IDs():
                 logging.info('Loading segmentation: ' + file)
-                self.segmentation_images[file[:-7]] = sitk.ReadImage(os.path.join(location, file))
-                self.segmentation_images[file[:-7]].CopyInformation(self.DICOM_manager.get_serie_image(file[:-7]))
+                folder = os.path.basename(location)
+                self.segmentation_images[folder][file[:-7]] = sitk.ReadImage(os.path.join(location, file))
+                self.segmentation_images[folder][file[:-7]].CopyInformation(self.DICOM_manager.get_serie_image(file[:-7]))
                 logging.info('Loaded segmentation: ' + file[:-7])
         self.mainframe.ITKviewer.update_images()
 
-    def load_segmentation(self, segmentation, serie_id):
-        if serie_id in self.segmentation_images:
+    def load_segmentation(self, segmentation, serie_id, name = "default"):
+        if name in self.segmentation_images[serie_id]:
             logging.warning('Segmentation name already exists, overwriting')
             
         if serie_id not in self.DICOM_manager.get_serie_IDs():
             logging.error('Serie ID not found')
             return
-        self.segmentation_images[serie_id] = segmentation
-        self.segmentation_images[serie_id].CopyInformation(self.DICOM_manager.get_serie_image(serie_id))
+        self.segmentation_images[serie_id][name] = segmentation
+        self.segmentation_images[serie_id][name].CopyInformation(self.DICOM_manager.get_serie_image(serie_id))
         self.segmentation_preview_images[serie_id] = None
         logging.info('Loaded segmentation: ' + serie_id)
         self.mainframe.ITKviewer.update_images()
 
 
-    def get_stats(self, serie_ID):
+    def get_stats(self, serie_ID, name = "default"):
         """placeholder"""
-        self.segmentation_stats[serie_ID] = sitk.LabelStatisticsImageFilter()
-        self.segmentation_stats[serie_ID].Execute(self.segmentation_images[serie_ID], self.DICOM_manager.get_serie_image(serie_ID))
-        return self.segmentation_stats[serie_ID]
+        self.segmentation_stats[serie_ID][name] = sitk.LabelStatisticsImageFilter()
+        self.segmentation_stats[serie_ID][name].Execute(self.segmentation_images[serie_ID], self.DICOM_manager.get_serie_image(serie_ID))
+        return self.segmentation_stats[serie_ID][name]
     
     def set_preview(self, serie_ID, preview: sitk.Image = None):
         if preview is None:
             logging.warning('No preview to set')
             return
-        self.segmentation_preview_images[serie_ID] = preview
+        self.segmentation_images[serie_ID]["preview"] = preview
 
     def get_preview(self, serie_ID):
-        if serie_ID not in self.segmentation_preview_images:
+        if "preview" not in self.segmentation_images[serie_ID]:
             logging.warning('No preview to get')
             return None
-        return self.segmentation_preview_images[serie_ID]
+        return self.segmentation_images[serie_ID]["preview"]
     
     def reset_preview(self, serie_ID):
-        self.segmentation_preview_images[serie_ID] = None
+        self.segmentation_images[serie_ID]["preview"] = None
 
     
-    def accept_preview(self, serie_ID):
-        if serie_ID not in self.segmentation_preview_images:
+    def accept_preview(self, serie_ID, name = "default"):
+        if serie_ID not in self.segmentation_images:
             logging.warning('No preview to accept')
             return
-        self.segmentation_images[serie_ID] = self.segmentation_preview_images[serie_ID]
+        self.segmentation_images[serie_ID][name] = self.segmentation_images[serie_ID]["preview"]
         self.reset_preview(serie_ID)
+
+    def get_label_dict(self, serie_ID):
+        if serie_ID not in self.seg_label_dict:
+            return None
+        return self.seg_label_dict[serie_ID]
+    
+    def set_label_dict(self, serie_ID, label_dict):
+        if not self.label_dict_is_valid(label_dict):
+            logging.warning('Invalid label dict')
+            return
+        self.seg_label_dict[serie_ID] = label_dict
+
+    def get_labels(self, serie_ID):
+        if serie_ID not in self.seg_label_dict:
+            return None
+        return self.seg_label_dict[serie_ID].keys()
+    
+    def get_label(self, serie_ID, label):
+        if serie_ID not in self.seg_label_dict:
+            return None
+        return self.seg_label_dict[serie_ID][label]
+    
+    def get_label_name(self, serie_ID, label):
+        self.get_label(serie_ID, label)
+
+    def set_label(self, serie_ID, label, name):
+        if serie_ID not in self.seg_label_dict:
+            self.seg_label_dict[serie_ID] = {}
+        if not isinstance(label, int):
+            logging.warning('Label must be an integer')
+            return
+        if not isinstance(name, str):
+            logging.warning('Name must be a string')
+            return
+        self.seg_label_dict[serie_ID][label] = name
+
+    def rename_label(self, serie_ID, label, name):
+        self.set_label(serie_ID, label, name)
+
+    def remove_label(self, serie_ID, label):
+        if serie_ID not in self.seg_label_dict:
+            return
+        if label not in self.seg_label_dict[serie_ID]:
+            return
+        del self.seg_label_dict[serie_ID][label]
